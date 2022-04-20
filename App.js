e2468821c45d852a95a4dcc08c7c7beac3f1e94a
gadaharsh <gada.hp@somaiya.edu>
2022-04-20 19:40:40 +0530

    title header

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleWrap}>
+        <Text style={styles.mainTitle}>Markets</Text>
+      </View>
+      <View style={styles.divider} />
     </View>
   );
 }
@@ -13,8 +15,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  mainTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  titleWrap: {
+    marginTop: 40,
+    paddingHorizontal: 16,
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "#A9ABB1",
+    marginHorizontal: 16,
+    marginTop: 16,
   },
 });
