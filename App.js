36ca3d59a3da3822e667e7015cbbc3d46d27a4c4
gadaharsh <gada.hp@somaiya.edu>
2022-04-24 00:47:41 +0530

    clean the code

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
+import React, { useCallback, useMemo, useRef } from 'react';
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList,SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
 import ListItem from "./components/ListItem";
 import { SAMPLE_DATA } from "./assets/data/sampleData";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
 
 const ListHeader = () => (
   <>
@@ -13,41 +18,43 @@ const ListHeader = () => (
 );
 
 export default function App() {
+
+  const bottomSheetModalRef = useRef(null);
+  const snapPoints = useMemo(() => ["50%"], []);
+
   return (
-    <SafeAreaView style={styles.container}>
-      {/* Header */}
-      {/* <View style={styles.titleWrap}>
-        <Text style={styles.mainTitle}>Markets</Text>
-      </View>
-      <View style={styles.divider} /> */}
+    <BottomSheetModalProvider>
+      <SafeAreaView style={styles.container}>
 
-      {/* <ListItem
-        name={SAMPLE_DATA[0].name}
-        symbol={SAMPLE_DATA[0].symbol}
-        currentPrice={SAMPLE_DATA[0].current_price}
-        priceChangePrecentage7d={
-          SAMPLE_DATA[0].price_change_percentage_7d_in_currency
-        }
-        logoUrl={SAMPLE_DATA[0].image}
-      /> */}
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={SAMPLE_DATA}
+          renderItem={({ item }) => (
+            <ListItem
+              name={item.name}
+              symbol={item.symbol}
+              currentPrice={item.current_price}
+              priceChangePrecentage7d={
+                item.price_change_percentage_7d_in_currency
+              }
+              logoUrl={item.image}
+            />
+          )}
+          ListHeaderComponent={<ListHeader />}
+        />
+      </SafeAreaView>
 
-      <FlatList
-        keyExtractor={(item) => item.id}
-        data={SAMPLE_DATA}
-        renderItem={({ item }) => (
-          <ListItem
-            name={item.name}
-            symbol={item.symbol}
-            currentPrice={item.current_price}
-            priceChangePrecentage7d={
-              item.price_change_percentage_7d_in_currency
-            }
-            logoUrl={item.image}
-          />
-        )}
-        ListHeaderComponent={<ListHeader/>}
-      />
-    </SafeAreaView>
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        snapPoints={snapPoints}
+        
+      >
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 }
 
